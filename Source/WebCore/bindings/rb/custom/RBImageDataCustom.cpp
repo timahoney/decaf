4fa579573ef1e4085c93532a90860dc3587cc88c
Tim Mahoney <tim.mahoney@me.com>
2013-04-23 11:30:56 -0700

    Marking ImageData as non-serializable for now.
    I'm having trouble with Ruby's Marshal module. The commented out
    serialization fails because the data becomes corrupt during
    un-marshaling. This should all be done with the structured clone
    anyway. I'll come back to this.

--- Source/WebCore/bindings/rb/custom/RBImageDataCustom.cpp
+++ Source/WebCore/bindings/rb/custom/RBImageDataCustom.cpp
@@ -27,6 +27,8 @@
 #include "RBImageData.h"
 
 #include "RBConverters.h"
+#include "RBImageDataCustom.h"
+#include "RBObject.h"
 #include "RBUint8ClampedArray.h"
 
 namespace WebCore {
@@ -34,37 +36,89 @@ namespace WebCore {
 extern "C" VALUE imagedata_get_data(VALUE self);
 VALUE imagedata_get_data(VALUE self)
 {
-    VALUE dataWrapper = rb_iv_get(self, "@data");
-    return dataWrapper;
+    ImageData* selfImpl = impl<ImageData>(self);
+    Uint8ClampedArray* data = selfImpl->data();
+    return toRB(data);
 }
 
-VALUE toRB(PassRefPtr<ImageData> prpImpl)
+VALUE toRB(PassRefPtr<ImageData> impl)
 {
-    RefPtr<ImageData> impl = prpImpl;
-    
     if (!impl)
         return Qnil;
 
-    // The JS bindings cache the data array directly on the JS object.
-    // That way, they don't always call into the C++ function to access this heavy object.
-    // I guess that makes sense, so let's do it for Ruby too.
-
-    // FIXME: This may have to change once we start caching the Ruby objects.
-    // The array may have already been wrapped,
-    // and the 'data' function may have already been mapped to the array.
-    Uint8ClampedArray* data = impl->data();
-    VALUE dataWrapper = toRB(data);
-    VALUE wrapper = toRB(RBImageData::rubyClass(), impl.release());
-    rb_iv_set(wrapper, "@data", dataWrapper);
-
-    static bool didRedefineMethod = false;
-    if (!didRedefineMethod) {
-        didRedefineMethod = true;
-        rb_undef_method(RBImageData::rubyClass(), "data");
+    // FIXME: The JS bindings cache the data array directly on the JS object.
+    // We can't do this at the moment because Ruby's Marshal module
+    // will try to serialize it. Instead, just define the 'data' getter.
+    static bool didDefineGetter = false;
+    if (!didDefineGetter) {
+        didDefineGetter = true;
         rb_define_method(RBImageData::rubyClass(), "data", RUBY_METHOD_FUNC(&imagedata_get_data), 0);
     }
     
-    return wrapper;
+    return toRB(RBImageData::rubyClass(), impl);
+}
+
+VALUE RBImageDataCustom::marshal_load(VALUE, VALUE data)
+{
+    UNUSED_PARAM(data);
+    
+    // FIXME: This doesn't work. The image data is corrupted.
+    
+    // VALUE divider = rb_str_new2("*");
+    // VALUE dataArray = rb_funcall(data, rb_intern("split"), 1, divider);
+    
+    // VALUE widthString = rb_ary_shift(dataArray);
+    // VALUE widthRB = rb_funcall(widthString, rb_intern("to_i"), 0);
+    // int width = NUM2INT(widthRB);
+    // VALUE heightString = rb_ary_shift(dataArray);
+    // VALUE heightRB = rb_funcall(heightString, rb_intern("to_i"), 0);
+    // int height = NUM2INT(heightRB);
+    // RefPtr<ImageData> result = ImageData::create(IntSize(width, height));
+    
+    // VALUE lengthString = rb_ary_shift(dataArray);
+    // VALUE lengthRB = rb_funcall(lengthString, rb_intern("to_i"), 0);
+    // unsigned length = NUM2UINT(lengthRB);
+    // VALUE restString = rb_ary_shift(dataArray);
+    // memcpy(result->data()->data(), RSTRING_PTR(restString), length);
+    
+    // return toRB(result.get());
+
+    return Qnil;
+}
+
+VALUE RBImageDataCustom::marshal_dump(VALUE self, VALUE)
+{
+    UNUSED_PARAM(self);
+    
+    // FIXME: This doesn't work. The data from imageDataString is corrupt after loading again.
+    
+    // ImageData* imageData = impl<ImageData>(self);
+    // VALUE dumpString = rb_str_new2("");
+    // VALUE divider = rb_str_new2("*");
+    // VALUE widthString = rb_funcall(INT2FIX(imageData->width()), rb_intern("to_s"), 0);
+    // rb_str_append(dumpString, widthString);
+    // rb_str_append(dumpString, divider);
+    // VALUE heightString = rb_funcall(INT2FIX(imageData->height()), rb_intern("to_s"), 0);
+    // rb_str_append(dumpString, heightString);
+    // rb_str_append(dumpString, divider);
+    
+    // unsigned length = imageData->data()->length();
+    // VALUE lengthString = rb_funcall(UINT2NUM(length), rb_intern("to_s"), 0);
+    // rb_str_append(dumpString, lengthString);
+    // rb_str_append(dumpString, divider);
+    // VALUE imageDataString = rb_str_new(buffer, length);
+    // rb_str_append(dumpString, imageDataString);
+    // return dumpString;
+    
+    rb_raise(rb_eArgError, "ImageData is not yet supported for serialized message data. Try passing its 'data' buffer instead.");
+    
+    return Qnil;
+}
+
+void RBImageDataCustom::Init_ImageDataCustom()
+{
+    rb_define_method(RBImageData::rubyClass(), "_dump", RUBY_METHOD_FUNC(&RBImageDataCustom::marshal_dump), 1);
+    rb_define_module_function(RBImageData::rubyClass(), "_load", RUBY_METHOD_FUNC(&RBImageDataCustom::marshal_load), 1);
 }
 
 } // namespace WebCore
--- Source/WebCore/bindings/rb/custom/RBImageDataCustom.h
+++ Source/WebCore/bindings/rb/custom/RBImageDataCustom.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2012 Tim Mahoney (tim.mahoney@me.com)
+ * Copyright (C) 2013 Tim Mahoney (tim.mahoney@me.com)
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -23,43 +23,21 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "config.h"
-#include "RBInitializationCustom.h"
+#ifndef RBImageDataCustom_h
+#define RBImageDataCustom_h
 
-#include "RBArrayBufferCustom.h"
-#include "RBBlobCustom.h"
-#include "RBConverters.h"
-#include "RBDataViewCustom.h"
-#include "RBFileCustom.h"
-#include "RBFileListCustom.h"
-#include "RBImageDataCustom.h"
-#include "RBMessagePortCustom.h"
-#include "RBWorker.h"
+#include <Ruby/ruby.h>
 
 namespace WebCore {
 
-extern "C" VALUE unimplemented(VALUE);
-
-void RBInitializationCustom::initializeCustomRubyClasses()
-{
-    RBArrayBufferCustom::Init_ArrayBufferCustom();
-    RBBlobCustom::Init_BlobCustom();
-    RBDataViewCustom::Init_DataViewCustom();
-    RBFileCustom::Init_FileCustom();
-    RBFileListCustom::Init_FileListCustom();
-    RBImageDataCustom::Init_ImageDataCustom();
-    RBMessagePortCustom::Init_MessagePortCustom();
+class RBImageDataCustom {
+public:
+    static VALUE marshal_load(VALUE klass, VALUE data);
+    static VALUE marshal_dump(VALUE self, VALUE level);
     
-    // Workers aren't implemented yet.
-    rb_define_singleton_method(RBWorker::rubyClass(), "new", RUBY_METHOD_FUNC(unimplemented), 0);
-}
-
-VALUE unimplemented(VALUE self)
-{
-    rb_raise(rb_eNotImpError,
-             "%s.%s is not implemented in Decaf at the moment.",
-             rbToString(self).utf8().data(),
-             rb_id2name(rb_frame_this_func()));
-}
+    static void Init_ImageDataCustom();
+};
 
 } // namespace WebCore
+
+#endif // RBImageDataCustom_h
